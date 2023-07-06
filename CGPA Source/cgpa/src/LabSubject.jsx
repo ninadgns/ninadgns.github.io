e7be4a19e1a14cf6cff4b6ae400464e35180ff39
Muhaiminul Islam Ninad <34864492+ninadgns@users.noreply.github.com>
2023-07-06 23:00:14 +0600

    fixed lab subject bold

--- CGPA Source/cgpa/src/LabSubject.jsx
+++ CGPA Source/cgpa/src/LabSubject.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import InputSlider from './InputSlider';
 import { Grid } from '@mui/material';
-import {Typography} from '@mui/material';
+import { Typography } from '@mui/material';
 export default function LabSubject({ SubjectName, returnGradePoint, examName }) {
     const [Incourse, setIncourse] = React.useState(85);
 
@@ -20,16 +20,16 @@ export default function LabSubject({ SubjectName, returnGradePoint, examName }) 
     returnGradePoint(gradePoint)
 
     return (
-        <Grid container sx={{borderStyle:'dashed', borderColor:'lightblue'}} marginBottom={1} padding={1}>
+        <Grid container sx={{ borderStyle: 'dashed', borderColor: 'lightblue' }} marginBottom={1} padding={1}>
             <Grid xs={12}>
                 <Typography variant='h5'>{SubjectName}</Typography>
             </Grid>
             <Grid xs={12}>
                 <InputSlider stepValue={5} defaultValue={85} maxValue={85} minValue={40} textVisibility='hidden' returnValue={setIncourse}></InputSlider>
             </Grid>
-            <Grid xs={12} sx={{marginTop:"-10px" ,marginBottom:"20px"}}>
-                <p>Grade: {grade}   </p>
-                <p>Grade Point: {gradePoint}</p>
+            <Grid xs={12} sx={{ marginTop: "-10px", marginBottom: "20px" }}>
+                <p>Grade: <b>{grade}</b>   </p>
+                <p>Grade Point: <b>{gradePoint}</b></p>
             </Grid>
             {/* <InputSlider examName={"Term Final"} defaultValue={68} maxValue={70} returnValue={setTermFinal}></InputSlider> */}
         </Grid>)
